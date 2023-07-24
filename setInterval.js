db2769f9a2e056bc7b83e68ac14229a0c0882a84
aashishrana <aashishrana71135@gmail.com>
2023-07-24 01:28:50 -0700

    Set intrval and setTimeout

---
+++ setInterval.js
@@ -0,0 +1,15 @@
+// setInterval(function oneMoreHello() {
+//     console.log("Hello Hites !");      // both are same
+// }, 1000);
+
+
+// overleaf
+
+function twoHello() {
+    console.log("Hii EveryOne");   // same above
+
+}
+
+// setInterval(twoHello, 2000);
+
+setTimeout(twoHello, 2000);
